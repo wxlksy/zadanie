f08ff9ac112a67b112f0eb8da05b4a884c8d8d93
wxlksy <31kiel12@gmail.com>
2025-02-15 14:45:45 +0300

    init

--- app/src/main/java/com/example/proverka/MainActivity.kt
+++ app/src/main/java/com/example/proverka/MainActivity.kt
@@ -3,30 +3,12 @@ package com.example.proverka
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
-import android.widget.Button
-import android.widget.TextView
+import com.example.proverka.databinding.ActivityMainBinding
 import kotlin.random.Random
 import kotlin.Boolean
 
-
 class MainActivity : AppCompatActivity()
 {
-    private lateinit var start_button: Button
-    private lateinit var true_button: Button
-    private lateinit var false_button: Button
-    private lateinit var textOperation: TextView
-    private lateinit var textFirstOperand: TextView
-    private lateinit var textSecondOperand: TextView
-    private lateinit var textEqually: TextView
-    private lateinit var textResult: TextView
-    private lateinit var textViewTrue: TextView
-    private lateinit var textViewTime: TextView
-    private lateinit var textViewPercentage: TextView
-    private lateinit var textViewTimeAvg: TextView
-    private lateinit var textNumberFalse: TextView
-    private lateinit var textViewTimeMax: TextView
-    private lateinit var textViewTimeMin: TextView
-
     private var startTime: Long = 0
     private var correctAnswers = 0
     private var totalAnswer = 0
@@ -34,40 +16,25 @@ class MainActivity : AppCompatActivity()
     private var minTime = Long.MIN_VALUE
     private var totalTime = 0L
 
-
+    private lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        start_button = findViewById(R.id.start_button)
-        true_button = findViewById(R.id.true_button)
-        false_button = findViewById(R.id.false_button)
-        textOperation = findViewById(R.id.textOperation)
-        textFirstOperand = findViewById(R.id.textFirstOperand)
-        textSecondOperand = findViewById(R.id.textSecondOperand)
-        textEqually = findViewById(R.id.textEqually)
-        textResult = findViewById(R.id.textResult)
-        textViewTrue = findViewById(R.id.textViewTrue)
-        textViewTime = findViewById(R.id.textViewTime)
-        textViewPercentage = findViewById(R.id.textViewPercentage)
-        textViewTimeAvg = findViewById(R.id.textViewTimeAvg)
-        textNumberFalse = findViewById(R.id.textNumberFalse)
-        textViewTimeMax = findViewById(R.id.textViewTimeMax)
-        textViewTimeMin = findViewById(R.id.textViewTimeMin)
-
-        start_button.setOnClickListener {
+        binding.startButton.setOnClickListener {
             generateExample()
-            true_button.isEnabled = true
-            false_button.isEnabled = true
+            binding.trueButton.isEnabled = true
+            binding.falseButton.isEnabled = true
             startTime = SystemClock.uptimeMillis()
         }
 
-        true_button.setOnClickListener {
+        binding.trueButton.setOnClickListener {
             checkAnswer(true)
         }
 
-        false_button.setOnClickListener {
+        binding.falseButton.setOnClickListener {
             checkAnswer(false)
         }
     }
@@ -78,9 +45,9 @@ class MainActivity : AppCompatActivity()
         val operand2 = Random.nextInt(10, 100)
         val operator = listOf('+', '-', '*', '/').random()
 
-        textFirstOperand.text = operand1.toString()
-        textSecondOperand.text = operand2.toString()
-        textOperation.text = operator.toString()
+        binding.textFirstOperand.text = operand1.toString()
+        binding.textSecondOperand.text = operand2.toString()
+        binding.textOperation.text = operator.toString()
 
         val correctAnswer = when (operator)
         {
@@ -106,7 +73,7 @@ class MainActivity : AppCompatActivity()
             }
         }
 
-        textResult.text = if (operator == '/')
+        binding.textResult.text = if (operator == '/')
         {
             String.format("%.2f", displayedAnswer)
         }
@@ -118,7 +85,7 @@ class MainActivity : AppCompatActivity()
 
     }
 
-    private fun checkAnswer(b: Boolean)
+    private fun checkAnswer(isCorrect: Boolean)
     {
         val elapsedTime = SystemClock.uptimeMillis() - startTime
         totalTime += elapsedTime
@@ -127,15 +94,29 @@ class MainActivity : AppCompatActivity()
         if (isCorrect)
         {
             correctAnswers++
-            textResult.text = "ПРАВИЛЬНО"
+            binding.textResult.text = "ПРАВИЛЬНО"
         }
 
         else
         {
-            textResult.text = "НЕ ПРАВИЛЬНО"
+            binding.textResult.text = "НЕ ПРАВИЛЬНО"
         }
 
-    }
+        minTime = minOf(minTime, elapsedTime)
+        maxTime = maxOf(maxTime, elapsedTime)
 
+        binding.textViewTime.text = "Время = $elapsedTime ms"
+        binding.textViewTrue.text = "Правильных ответов = $correctAnswers"
+        binding.textViewFalse.text = "Неправильных ответов ${totalAnswer - correctAnswers}"
+        binding.textViewPercentage.text = "Процент правильных = ${String.format("%.2f", (correctAnswers.toDouble() / totalAnswer * 100))}%"
+        binding.textViewTimeMax.text = "Максимальное время: $maxTime ms"
+        binding.textViewTimeMin.text = "Минимальное время: $minTime ms"
+        binding.textViewTimeAvg.text = "Среднее время: ${String.format("%.2f", totalTime.toDouble() / totalAnswer)} ms"
+
+        binding.trueButton.isEnabled = false
+        binding.falseButton.isEnabled = false
+
+    }
 }
+
 
